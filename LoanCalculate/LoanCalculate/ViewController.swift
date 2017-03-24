aff62769573d9e4ff1a56a2f3b98a83e8e0793df
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-3.local>
2017-03-24 19:52:24 +0800

    贷款计算器

--- LoanCalculate/LoanCalculate/ViewController.swift
+++ LoanCalculate/LoanCalculate/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loanAmountfield: UITextField!
+    
+    @IBOutlet weak var loanTermField: UITextField!
+    
+    @IBOutlet weak var interestRateField: UITextField!
+    
+    
+
+    
+    @IBOutlet weak var Result: UILabel!
+    
+    @IBAction func simpleButton(_ sender: Any) {
+    let calculate1 = simpleCalculate()
+    Result.text=calculate1.calculate(loanAmount:Double(loanAmountfield.text!)!,years:Int(loanTermField.text!)!,interestRate:Double(interestRateField.text!)!).description
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+class simpleCalculate {
+    func calculate(loanAmount:Double,years:Int,interestRate:Double)->Double{
+    let rate = interestRate/100
+    let interest = loanAmount * rate * Double(years)
+    return loanAmount + interest
+  }
+}
